bf37cb0eac78f8c33438e49fcfbb3fe01cb461e5
lbq-cn <50507718+lbq-cn@users.noreply.github.com>
2024-06-02 22:27:51 +0800

    Update my-script.js

--- my-script.js
+++ my-script.js
@@ -1,4 +1,4 @@
-$httpClient.get('https://api.my-ip.io/ip', function (error, response, data) {
+$httpClient.get('https://api64.ipify.org?format=json', function (error, response, data) {
   $done({
     title: '当前 IP 地址',
     content: data,
