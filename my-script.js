b372f0b1e2876a55066973dc41b62ea9fc5d8322
lbq-cn <50507718+lbq-cn@users.noreply.github.com>
2024-06-02 22:32:21 +0800

    Update my-script.js

--- my-script.js
+++ my-script.js
@@ -1,8 +1,19 @@
 $httpClient.get('https://api64.ipify.org?format=json', function (error, response, data) {
-  $done({
-    title: '当前 IP 地址',
-    content: data,
-    backgroundColor: '#663399',
-    icon: 'network',
-  });
+  if (error) {
+    console.log('API请求失败:', error);
+    $done({
+      title: '当前 IP 地址',
+      content: '获取 IP 地址失败',
+      backgroundColor: '#663399',
+      icon: 'network',
+    });
+  } else {
+    var ipData = JSON.parse(data);
+    $done({
+      title: '当前 IP 地址',
+      content: ipData.ip,
+      backgroundColor: '#663399',
+      icon: 'network',
+    });
+  }
 });
