874c37edb36ce08bf50fb91c41c5a6508175c67c
lbq-cn <50507718+lbq-cn@users.noreply.github.com>
2024-06-02 22:51:10 +0800

    Update my-script.js

--- my-script.js
+++ my-script.js
@@ -11,7 +11,7 @@ $httpClient.get('https://ipinfo.io/json', function (error, response, data) {
     var ipData = JSON.parse(data);
     $done({
       title: '当前 IP 和 位置',
-      content: `IP: ${ipData.ip}\n城市: ${ipData.city}\n地区: ${ipData.region}\n国家: ${ipData.country}}`,
+      content: `IP: ${ipData.ip}\n城市: ${ipData.city}\n地区: ${ipData.region}\n国家: ${ipData.country}`,
       backgroundColor: '#663399',
       icon: 'network',
     });
